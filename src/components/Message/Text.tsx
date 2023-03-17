24dc9b6bf7d1dd26faf063c86f424cc7fc99de9c
Matias Benary <matiasbenary@gmail.com>
2023-03-17 20:29:25 -0300

    :pencil: Se agrego mensaje y conexion de api

--- src/components/Message/Text.tsx
+++ src/components/Message/Text.tsx
@@ -1,13 +1,53 @@
+import { apiCall } from 'lib/api'
+import { useEffect, useState } from 'react'
+import Error from './Error'
+
 type TextProps = {
   code: string
 }
 
+type CertificateResponse = {
+  name: string
+  id: number
+  id_number: string
+  title: string
+  certificate_description: string | null
+  horas: string | null
+  activity_from: string | null
+  activity_to: string | null
+  creatorName: string | null
+  bussiness_name: string | null
+}
+
 const Text = ({ code }: TextProps) => {
+  const [certificate, setCertificate] = useState<CertificateResponse>()
+
+  const getData = async () => {
+    const response = await apiCall(
+      `getCertificateDate/${code.toUpperCase()}`,
+      null,
+      'GET'
+    )
+    setCertificate(response.data?.certificate)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  if (certificate === undefined) {
+    return <p> Cargando...</p>
+  }
+
+  if (certificate === null) {
+    return <Error></Error>
+  }
+
   return (
     <div className="bg-gray-100 py-5 pl-6">
       <p>
-        Esta credencial fue entregada a Daniela Zamudio el 16 de diciembre de
-        2021
+        Este certificado fue emitido a {certificate.name} avalado por la entidad{' '}
+        {certificate.bussiness_name}
       </p>
       <p> Código: {code}</p>
     </div>
